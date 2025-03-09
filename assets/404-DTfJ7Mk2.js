61d37c0395ee734f7aadde7f31300baad72092ef
aixinxino <2387840095@QQ.COM>
2025-03-10 00:19:50 +0800

    Updates

---
+++ assets/404-DTfJ7Mk2.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Exception-B4SOUHks.js";import{d as s,J as e,y as a,o,u as n}from"./vendor-DfTV9N1v.js";import"./index-B549kHEA.js";const r=s({__name:"404",setup(s){const r=e({title:"404",desc:"抱歉，你访问的页面不存在",btnText:"返回首页",imgUrl:"/TestDemo/assets/404-Zjuae4tx.png"});return(s,e)=>{const m=t;return o(),a(m,{data:n(r)},null,8,["data"])}}});export{r as default};
--- assets/lock_screen_1-YWvwm5PZ.js
+++
@@ -1,1 +0,0 @@
-const s="/TestDemo/assets/lock_screen_1-DQPth8oz.png";export{s as l};
